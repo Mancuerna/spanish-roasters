c9c022edcb5d028cb91dcafbcbc24f4032111bf3
Mancuerna <jacortesibanez@gmail.com>
2023-09-10 22:08:47 +0000

    Get coffee origins URL and filter the wrong ones

--- crawlers/cafes_zaidin/cafes_zaidin.mjs
+++ crawlers/cafes_zaidin/cafes_zaidin.mjs
@@ -1,15 +1,30 @@
-import { PlaywrightCrawler } from "crawlee";
+import { PlaywrightCrawler, log } from "crawlee";
+
+log.setLevel(log.LEVELS.DEBUG);
 
 const crawler = new PlaywrightCrawler({
-  requestHandler: async ({ page }) => {
-    await page.waitForSelector('div[class="grid-category__card"]');
-    const coffeeOrigins = await page.$$eval(
-      'div[class="grid-category__title-inner"]',
-      (els) => {
-        return els.map((el) => el.textContent);
-      }
-    );
+  requestHandler: async ({ page, request, enqueueLinks }) => {
+    log.debug(`Proccessing: ${request.url}`);
+    if (request.label === "ORIGIN") {
+    } else {
+      await page.waitForSelector('div[class="grid-category__card"]');
+      await enqueueLinks({
+        transformRequestFunction(req) {
+          return filterWrongOrigins(req);
+        },
+        selector: 'a[class="grid-category__title"]',
+        label: "ORIGIN",
+      });
+    }
   },
 });
+
+const filterWrongOrigins = (req) => {
+  const WRONG_ORIGINS = ["Infusiones", "Gama-de-caf", "Blends"];
+  if (WRONG_ORIGINS.some((element) => req.url.includes(element))) {
+    return false;
+  }
+  return req;
+};
 
 await crawler.run(["https://www.soycafetera.es/tienda"]);
