ecfe32fab900d3d7918a498be28621c00e109f0d
Mancuerna <jacortesibanez@gmail.com>
2023-09-11 17:34:42 +0000

    Add logic to crawl each coffee link

--- crawlers/cafes_zaidin/cafes_zaidin.mjs
+++ crawlers/cafes_zaidin/cafes_zaidin.mjs
@@ -1,11 +1,16 @@
 import { PlaywrightCrawler, log } from "crawlee";
 
-log.setLevel(log.LEVELS.DEBUG);
+log.setLevel(log.LEVELS.INFO);
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page, request, enqueueLinks }) => {
-    log.debug(`Proccessing: ${request.url}`);
+    log.info(`Proccessing: ${request.url}`);
     if (request.label === "ORIGIN") {
+      await page.waitForSelector('a[class="breadcrumbs__link ec-link ec-link--muted "]');
+      await enqueueLinks({
+        selector: 'a[class="grid-product__title"]',
+        label: "COFFEE",
+      });
     } else {
       await page.waitForSelector('div[class="grid-category__card"]');
       await enqueueLinks({
