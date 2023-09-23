dc51dd874a47e7a7799eb904ccb9016818d66152
Mancuerna <jacortesibanez@gmail.com>
2023-09-23 21:03:17 +0000

    Add disconnect and getCoffee method to db controller

--- prisma/database_controller.mjs
+++ prisma/database_controller.mjs
@@ -1,8 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-export const insertCoffee = async (coffee) => {
-  coffee = {data: coffee};
-  await prisma.coffee_roasters.create(coffee);
-  await prisma.$disconnect();
-};
+
+const insertCoffee = async (coffee) => {
+    coffee = { data: coffee };
+    await prisma.coffee_roasters.create(coffee);
+    disconnect();
+  },
+  disconnect = async () => {
+    await prisma.$disconnect();
+  },
+  getCoffee = async (coffeeUrl) => {
+    return await prisma.coffee_roasters.findFirst({
+      where: { url: coffeeUrl },
+    });
+  };
+
+export { insertCoffee, disconnect, getCoffee };
