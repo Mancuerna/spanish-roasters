77fb41b4064023700eaa2f6d582421850e1150ee
Mancuerna <jacortesibanez@gmail.com>
2023-09-30 22:17:24 +0000

    Create myTypes to add some custom types

---
+++ prisma/myTypes.ts
@@ -0,0 +1,15 @@
+type Coffee = {
+  url: string;
+  coffeeName: string;
+  roasterName: string;
+  region: string;
+  farm: string;
+  proccess: string;
+  altitude: string;
+  varietal: string;
+  tastingNotes: string;
+  coffeeImage: string;
+  dateAdded: number;
+  active: boolean;
+};
+export { Coffee };
