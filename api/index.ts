beb4a469b326ebd5c05c2c0d50c822a82cac3cd5
Mancuerna <jacortesibanez@gmail.com>
2023-10-07 20:41:41 +0000

    Add basic /coffee endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import { getCoffee } from "../prisma/database_controller";
 const app = express();
 app.use(express.json());
+
+app.get(`/coffee`, async (req, res) => {
+  const coffeeUrl: string = req.query.coffeUrl as string;
+  const coffeeData = await getCoffee(coffeeUrl);
+  if (coffeeData !== null) res.status(200).json(coffeeData);
+  else res.status(404).json({ error: "Coffee not found" });
+});
+
 app.listen(3000, () =>
   console.log(`🚀 Server ready at: http://localhost:3000`)
 );
