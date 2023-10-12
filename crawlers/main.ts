5542e83fc37b2a80954168ecda45d391b8897187
Mancuerna <jacortesibanez@gmail.com>
2023-10-12 10:48:32 +0000

    Use API endpoint to insert coffees

--- crawlers/main.ts
+++ crawlers/main.ts
@@ -1,6 +1,8 @@
 import { crawler as zaidinCrawler } from "./cafes_zaidin/main";
 import { log, purgeDefaultStorages } from "crawlee";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 log.setLevel(log.LEVELS.INFO);
 log.debug("Setting up crawler.");
 
